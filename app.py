2d93c6fa2b589e74ffb5e2acaefb98496dcc776e
Shabucode <shabnamb4u@gmail.com>
2025-03-13 13:00:21 +0000

    Create Project

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 os.environ['GROQ_API_KEY'] = os.getenv('GROQ_API_KEY')
-
 os.environ["AUTOGEN_USE_DOCKER"]="False"
 
 llm_config = {
@@ -37,3 +36,61 @@ print(question)
 schema = info["schema"]
 print(schema)
 
+#Step 3: Agent Implementation
+# config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
+
+def check_termination(msg: Dict):
+    if "tool_responses" not in msg:
+        return False
+    json_str = msg["tool_responses"][0]["content"]
+    obj = json.loads(json_str)
+    return "error" not in obj or obj["error"] is None and obj["reward"] == 1
+
+#Using Autogen,  a SQL agent can be implemented with a ConversableAgent.
+sql_writer = ConversableAgent(
+    "sql_writer",
+    llm_config=llm_config,
+    system_message="You are good at writing SQL queries. Always respond with a function call to execute_sql().",
+    is_termination_msg=check_termination,
+)
+
+user_proxy = UserProxyAgent(
+    "user_proxy",
+    human_input_mode="NEVER",
+    max_consecutive_auto_reply=5
+)
+
+#Step 4: Create Tools / Function Calling with decorator
+#The gym environment executes the generated SQL query and 
+# the agent can take execution results as feedback to improve 
+# its generation in multiple rounds of conversations.
+
+@sql_writer.register_for_llm(description="Function for executing SQL query and returning a response")
+@user_proxy.register_for_execution()
+
+def execute_sql(
+    reflection: Annotated[str, "Think about what to do"], sql: Annotated[str, "SQL query"]
+    ) -> Annotated[Dict[str, str], "Dictionary with keys 'result' and 'error'"]:
+    observation, reward, _, _, info = gym.step(sql)
+    error = observation["feedback"]["error"]
+    if not error and reward ==0:
+        error = "The SQL query returned an incorrect result"
+    if error:
+        return {
+            "error": error,
+            "wrong_result": observation["feedback"]["result"],
+            "correct_result": info["gold_result"],
+        }
+    else:
+        return {
+            "result": observation["feedback"]["result"],
+        }
+#The agent can then take as input the schema and the text question and 
+#generate the SQL query
+
+prompt_message = f"""Below is the schema for a SQL database:
+{schema}
+Generate a SQL query to answer the following question:
+{question}"""
+
+user_proxy.initiate_chat(sql_writer, message=prompt_message)
